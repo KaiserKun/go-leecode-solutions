d26994499cb454d717a93faaccc4d97f53c65f2d
xiaokunji <1626438270@qq.com>
2025-10-25 22:31:16 +0800

    奇偶频次间的最大差值 II

---
+++ hard/maximum-difference-between-even-and-odd-frequency-ii.go
@@ -0,0 +1,33 @@
+package hard
+
+import "math"
+
+// link: https://leetcode.cn/problems/maximum-difference-between-even-and-odd-frequency-ii/
+// thought: 滑动窗口，维护一个窗口内的字符频率，每个窗口，计算奇数和偶数频率的差值
+func maxDifference(s string, k int) int {
+	const inf = math.MaxInt / 2
+	ans := -inf
+	for x := range 5 {
+		for y := range 5 {
+			if y == x {
+				continue
+			}
+			curS := [5]int{}
+			preS := [5]int{}
+			minS := [2][2]int{{inf, inf}, {inf, inf}}
+			left := 0
+			for i, b := range s {
+				curS[b-'0']++
+				r := i + 1
+				for r-left >= k && curS[x] > preS[x] && curS[y] > preS[y] {
+					p := &minS[preS[x]&1][preS[y]&1]
+					*p = min(*p, preS[x]-preS[y])
+					preS[s[left]-'0']++
+					left++
+				}
+				ans = max(ans, curS[x]-curS[y]-minS[curS[x]&1^1][curS[y]&1])
+			}
+		}
+	}
+	return ans
+}
