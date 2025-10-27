44b270f846a576f4b36f8b46063e3cee165304ea
xiaokunji <1626438270@qq.com>
2025-10-27 11:28:48 +0800

    长度最小的子数组

---
+++ topics/sliding-window/minimum-size-subarray-sum.go
@@ -0,0 +1,22 @@
+package slidingwindow
+
+// link: https://leetcode.cn/problems/minimum-size-subarray-sum/
+// thought: 滑动窗口，左指针扩大窗口，右指针缩小窗口
+func minSubArrayLen(target int, nums []int) int {
+	n := len(nums)
+	ans, left, right := n+1, n-1, n-1
+	sum := 0
+	for left >= 0 {
+		sum += nums[left]
+		for sum >= target {
+			ans = min(ans, right-left+1)
+			sum -= nums[right]
+			right--
+		}
+		left--
+	}
+	if ans > n {
+		return 0
+	}
+	return ans
+}
