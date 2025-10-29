d377adb71b88026a689751454b943072dffa8f61
xiaokunji <1626438270@qq.com>
2025-10-29 11:06:49 +0800

    字典序的第K小数字

---
+++ topics/trie/k-th-smallest-in-lexicographical-order.go
@@ -0,0 +1,29 @@
+package trie
+
+// link: https://leetcode.cn/problems/k-th-smallest-in-lexicographical-order/
+// thought: 按前缀树组织，重点在于计算当前子树的数量
+func findKthNumber(n int, k int) int {
+	cur := 1
+	k--
+	for k > 0 {
+		steps := calc(n, cur, cur+1)
+		if steps <= k {
+			cur++
+			k -= steps
+		} else {
+			cur *= 10
+			k--
+		}
+	}
+	return cur
+}
+
+func calc(n, cur, next int) int {
+	steps := 0
+	for cur <= n {
+		steps += min(n+1, next) - cur
+		cur *= 10
+		next *= 10
+	}
+	return steps
+}
