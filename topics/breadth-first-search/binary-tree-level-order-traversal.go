b7e80c712d60794a92eebd099af6893b965e04c9
xiaokunji <1626438270@qq.com>
2025-11-06 15:59:59 +0800

    二叉树的层序遍历

---
+++ topics/breadth-first-search/binary-tree-level-order-traversal.go
@@ -0,0 +1,37 @@
+package bfs
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// link: https://leetcode.cn/problems/binary-tree-level-order-traversal/
+// thought: 层序遍历的标准实现，bfs
+func levelOrder(root *TreeNode) [][]int {
+	ans := [][]int{}
+	if root == nil {
+		return ans
+	}
+
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		size := len(q)
+		level := []int{}
+
+		for i := 0; i < size; i++ {
+			node := q[0]
+			q = q[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+
+		ans = append(ans, level)
+	}
+	return ans
+}
