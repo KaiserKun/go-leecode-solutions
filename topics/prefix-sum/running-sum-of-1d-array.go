62ecbd70ce74df2d56f7c9846a08a35d62d4d41a
xiaokunji <1626438270@qq.com>
2025-11-19 17:32:19 +0800

    一维数组的动态和

---
+++ topics/prefix-sum/running-sum-of-1d-array.go
@@ -0,0 +1,11 @@
+package ps
+
+// link: https://leetcode.cn/problems/running-sum-of-1d-array/
+// thought: 直接在原数组修改即可
+func runningSum(nums []int) []int {
+	n := len(nums)
+	for i := 1; i < n; i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
+}
