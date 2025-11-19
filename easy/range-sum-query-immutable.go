f135cab486a836b7eee55c8ce29fdfe86ab42920
xiaokunji <1626438270@qq.com>
2025-11-19 17:24:14 +0800

    区域和检索 - 数组不可变

---
+++ easy/range-sum-query-immutable.go
@@ -0,0 +1,29 @@
+package easy
+
+// link: https://leetcode.cn/problems/range-sum-query-immutable/
+// thought: 利用数组prefix[i]记录前i个元素的和
+
+type NumArray struct {
+	prefix []int
+}
+
+func Constructor(nums []int) NumArray {
+	n := len(nums)
+	prefix := make([]int, n+1)
+	prefix[0] = 0
+	for i := 0; i < n; i++ {
+		prefix[i+1] = prefix[i] + nums[i]
+	}
+
+	return NumArray{prefix}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	return this.prefix[right+1] - this.prefix[left]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
