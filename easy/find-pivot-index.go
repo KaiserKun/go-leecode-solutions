693717237d887b760295bc2f70fed395d98b1185
xiaokunji <1626438270@qq.com>
2025-11-20 20:49:17 +0800

    寻找数组的中心下标

---
+++ easy/find-pivot-index.go
@@ -0,0 +1,18 @@
+package easy
+
+// link: https://leetcode.cn/problems/find-pivot-index/
+// thought: 前缀和，左侧元素和就是前缀和，右侧元素和为prefix[n]-prefix[i+1]
+func pivotIndex(nums []int) int {
+	n := len(nums)
+	prefix := make([]int, n+1)
+	prefix[0] = 0
+	for i := 0; i < n; i++ {
+		prefix[i+1] = prefix[i] + nums[i]
+	}
+	for i := 0; i < n; i++ {
+		if prefix[i] == prefix[n]-prefix[i+1] {
+			return i
+		}
+	}
+	return -1
+}
