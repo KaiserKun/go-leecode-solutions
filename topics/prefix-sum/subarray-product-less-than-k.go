c30443552f0fbd7fe25835547d9aef3887145915
xiaokunji <1626438270@qq.com>
2025-11-24 08:56:27 +0800

    乘积小于 K 的子数组

---
+++ topics/prefix-sum/subarray-product-less-than-k.go
@@ -0,0 +1,22 @@
+package ps
+
+// link: https://leetcode.cn/problems/subarray-product-less-than-k/
+// thought: 左右指针 + 前缀和
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	if k <= 1 {
+		return 0
+	}
+	n := len(nums)
+	pro, left, right := 1, 0, 0
+	ans := 0
+	for right < n {
+		pro *= nums[right]
+		for pro >= k {
+			pro /= nums[left]
+			left++
+		}
+		ans += right - left + 1
+		right++
+	}
+	return ans
+}
